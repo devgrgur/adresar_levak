0821030e40e30f08829abaae5f9d44ba66d217c2
devgrgur <devgrgur03@gmail.com>
2025-02-10 04:41:27 +0100

    #1/ project init

---
+++ src/main/java/io/github/devgrgur/adresar_levak/AdresarLevakApplication.java
@@ -0,0 +1,13 @@
+package io.github.devgrgur.adresar_levak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdresarLevakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdresarLevakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devgrgur/adresar_levak/AdresarLevakApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devgrgur.adresar_levak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdresarLevakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
