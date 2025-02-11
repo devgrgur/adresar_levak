2f8c6c10ff5787b4ed54bf51d741a65c7127d672
devgrgur <devgrgur03@gmail.com>
2025-02-11 15:13:11 +0100

    #2/ basic database setup, added schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+CREATE TYPE gender_type AS ENUM ('male', 'female', 'other');
+
+CREATE TABLE IF NOT EXISTS person
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    pin        VARCHAR(11) NOT NULL UNIQUE,
+    gender     gender_type NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS person_phone_number
+(
+    id           SERIAL PRIMARY KEY,
+    person_id    INTEGER NOT NULL,
+    phone_number VARCHAR(10),
+    FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS person_email_address
+(
+    id            SERIAL PRIMARY KEY,
+    person_id     INTEGER NOT NULL,
+    email_address VARCHAR(255),
+    FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+);
